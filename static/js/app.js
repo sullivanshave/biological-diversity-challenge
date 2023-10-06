78635a2a6a1abfd3e46fc57ad8cc875dfe4a36a5
sullivanshave@gmail.com <sullivanshave@gmail.com>
2023-10-05 20:52:22 -0500

    updated app.js file

--- static/js/app.js
+++ static/js/app.js
@@ -14,11 +14,115 @@ function init() {
     });
 
     let firstSample = data.names[0];
-    buildCharts(firstSample);
+    console.log(firstSample);
+
+
+    buildBarChart(firstSample);
     buildMetadata(firstSample);
-    buildGauge(firstSample);
-    buildBubble(firstSample);
+    buildBubbleChart(firstSample);
 };
 
 
-function buildMetadata(sample) {}
+function buildBarChart(sample) {
+    d3.json(url).then(function(data) {
+        let samples = data.samples;
+        let resultArray = samples.filter(function(data) {
+            return data.id === sample;
+        });
+
+        let result = resultArray[0];
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        console.log(otu_ids);
+        console.log(otu_labels);
+        console.log(sample_values);
+
+        let yticks = otu_ids.slice(0, 10).map(function(otuID) {
+            return `OTU ${otuID}`;
+        }).reverse();
+
+        let xticks = sample_values.slice(0, 10).reverse();
+
+        let labels = otu_labels.slice(0, 10).reverse();
+
+        let trace = {
+            x: xticks,
+            y: yticks,
+            text: labels,
+            type: "bar",
+            orientation: "h"
+        };
+
+        let layout = {
+            title: "Top 10 OTUs Found per Individual",
+            margin: { t: 30, l: 150 }
+        };
+
+        Plotly.newPlot("bar", [trace], layout);
+
+});} 
+
+function buildMetadata(sample) {
+    d3.json(url).then(function(data) {
+        let metadata = data.metadata;
+        let resultArray = metadata.filter(function(data) {
+            return data.id === sample;
+        });
+        let result = resultArray[0];
+        let PANEL = d3.select("#sample-metadata");
+        PANEL.html("");
+        Object.entries(result).forEach(function([key, value]) {
+            PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
+    });
+};
+
+
+function buildBubbleChart(sample) {
+    d3.json(url).then(function(data) {
+        let samples = data.samples;
+        let resultArray = samples.filter(function(data) {
+            return data.id === sample;
+        });
+
+        let result = resultArray[0];
+        let otu_ids = result.otu_ids;
+        let otu_labels = result.otu_labels;
+        let sample_values = result.sample_values;
+
+        let trace = {
+            x: otu_ids,
+            y: sample_values,
+            mode: "markers",
+            marker: {
+                size: sample_values,
+                color: otu_ids
+            },
+            text: otu_labels
+        };
+
+        let layout = {
+            title: "Bacteria Cultures per Sample",
+            xaxis: { title: "OTU ID" },
+            height: 600,
+            width: 1000
+        };
+
+        Plotly.newPlot("bubble", [trace], layout);
+    });
+
+
+}
+
+function optionChanged(value) { 
+
+    console.log(value); 
+
+    buildMetadata(value);
+    buildBarChart(value);
+    buildBubbleChart(value);
+};
+
+init();
